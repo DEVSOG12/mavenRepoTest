90c904b296118f2702203bd014846e8d9e0fc813
DEVSOG12 <oreofesolarin@gmail.com>
2023-07-17 02:16:01 -0400

    Multiprocessing adjustment feature

--- GoRepro/main.py
+++ GoRepro/main.py
@@ -1,7 +1,19 @@
+import random
+
 import requests
 import json
 from git import Repo
 import os
+from multiprocessing import Pool
+
+def run_reprotest(repoInfo, resultFileName, variation):
+    command = "sudo reprotest --diffoscope-arg='--html=/home/osolarin/ReproducibleTests/GoRepro/" \
+              "data/diffoscopeLogs{}/{}/{}.html' --variations=+{} " \
+              "\"go build -mod=mod -modcacherw -ldflags '-s -w -extldflags=-Z' -trimpath -o dist/bin\" " \
+              "'dist/*'".format(resultFileName, repoInfo[0].replace("/", "_"), variation, variation)
+    print(command)
+    exit_code = os.system(command)
+    return variation if exit_code == 0 else None
 
 
 def fixStars():
@@ -67,8 +79,7 @@ def testGoReproducible(repoInfo, resultFileName):
     possible_variations = ["environment", "build_path", "user_group.available+=builduser:builduser", "fileordering",
                            "home", "kernel", "locales", "exec_path", "time", "timezone", "umask"]
 
-    variations_reproducible = []
-    variations_not_reproducible = []
+
 
     try:
         repo = Repo.clone_from(repoInfo[1], '/home/osolarin/ReproducibleTests/GoRepro/data/repos/{}'.format(repoInfo[0].replace("/", "_")))
@@ -117,46 +128,108 @@ def testGoReproducible(repoInfo, resultFileName):
         return [True, "Fully Reproducible"]
     else:
         # Try all other variations
-        for variation in possible_variations:
-            command = "sudo reprotest --diffoscope-arg='--html=/home/osolarin/ReproducibleTests/GoRepro/" \
-                      "data/diffoscopeLogs{}/{}/all.html' --variations=+{} " \
-                      "\"go build -mod=mod -modcacherw -ldflags '-s -w -extldflags=-Z' -trimpath -o dist/bin\" " \
-                      "'dist/*'".format(resultFileName, repoInfo[0].replace("/", "_"), variation)
-            print(command)
-            exit_code = os.system(command)
-            if exit_code == 0:
-                variations_reproducible.append(variation)
+        variations_reproducible = []
+        variations_not_reproducible = []
+
+        # Use multiprocessing to run reprotest commands in parallel
+        pool = Pool()
+        results = pool.starmap(run_reprotest, [(repoInfo, resultFileName, variation) for variation in possible_variations])
+        pool.close()
+        pool.join()
+
+        for result in results:
+            if result is not None:
+                variations_reproducible.append(result)
             else:
-                variations_not_reproducible.append(variation)
+                variations_not_reproducible.append(result)
 
-        # Record the results
-        record = json.loads(
-            open('/home/osolarin/ReproducibleTests/GoRepro/data/{}.json'.format(resultFileName), 'r').read())["results"]
-        record.append({
-            "project": repoInfo[0],
-            "stars": repoInfo[2],
-            "status": "Partially Reproducible" if len(variations_reproducible) > 0 else "Not Reproducible",
-            "variationsNonReproducible": variations_not_reproducible,
-            "variationsReproducible": variations_reproducible
-        })
+                # Record the results
+                record = json.loads(
+                    open('/home/osolarin/ReproducibleTests/GoRepro/data/{}.json'.format(resultFileName), 'r').read())[
+                    "results"]
+                record.append({
+                    "project": repoInfo[0],
+                    "stars": repoInfo[2],
+                    "status": "Partially Reproducible" if len(variations_reproducible) > 0 else "Not Reproducible",
+                    "variationsNonReproducible": variations_not_reproducible,
+                    "variationsReproducible": variations_reproducible
+                })
 
-        print(record)
+                print(record)
 
-        with open('/home/osolarin/ReproducibleTests/GoRepro/data/{}.json'.format(resultFileName), 'w') as f:
-            json.dump({"results": record}, f)
+                with open('/home/osolarin/ReproducibleTests/GoRepro/data/{}.json'.format(resultFileName), 'w') as f:
+                    json.dump({"results": record}, f)
 
-        return [True, "Partially Reproducible" if len(variations_reproducible) > 0 else "Not Reproducible"]
+                return [True, "Partially Reproducible" if len(variations_reproducible) > 0 else "Not Reproducible"]
+        #
+        # for variation in possible_variations:
+        #     command = "sudo reprotest --diffoscope-arg='--html=/home/osolarin/ReproducibleTests/GoRepro/" \
+        #               "data/diffoscopeLogs{}/{}/all.html' --variations=+{} " \
+        #               "\"go build -mod=mod -modcacherw -ldflags '-s -w -extldflags=-Z' -trimpath -o dist/bin\" " \
+        #               "'dist/*'".format(resultFileName, repoInfo[0].replace("/", "_"), variation)
+        #     print(command)
+        #     exit_code = os.system(command)
+        #     if exit_code == 0:
+        #         variations_reproducible.append(variation)
+        #     else:
+        #         variations_not_reproducible.append(variation)
+        #
+        # # Record the results
+        # record = json.loads(
+        #     open('/home/osolarin/ReproducibleTests/GoRepro/data/{}.json'.format(resultFileName), 'r').read())["results"]
+        # record.append({
+        #     "project": repoInfo[0],
+        #     "stars": repoInfo[2],
+        #     "status": "Partially Reproducible" if len(variations_reproducible) > 0 else "Not Reproducible",
+        #     "variationsNonReproducible": variations_not_reproducible,
+        #     "variationsReproducible": variations_reproducible
+        # })
+        #
+        # print(record)
+        #
+        # with open('/home/osolarin/ReproducibleTests/GoRepro/data/{}.json'.format(resultFileName), 'w') as f:
+        #     json.dump({"results": record}, f)
+        #
+        # return [True, "Partially Reproducible" if len(variations_reproducible) > 0 else "Not Reproducible"]
+
+
+def convert_csv():
+    with open('data/go_Random400Results.json', 'r') as f:
+        records = json.load(f)['results']
+
+        # If len(records) > 400: records = records[:400]
+
+        if len(records) > 400:
+            # Radomly select 400
+            records = random.sample(records, 400)
+
+        s = open('data/go_Random400Results.csv', 'w')
+        s.write("project, status, reproducibleVariants, nonReproducibleVariants, stars\n")
+        for i in range(len(records)):
+            s.write("{}, {}, {}, {}, {}\n".format(
+                records[i]['project'],
+                records[i]['status'],
+                str(records[i]['variationsReproducible']).replace(',', ';'),
+                str(records[i]['variationsNonReproducible']).replace(',', ';'),
+                records[i]['stars']))
+        s.close()
+
+        # Save the randomly sampled records
+        with open('data/go_Random400Results.json', 'w') as f:
+            json.dump({"results": records}, f)
+
 
 
 if __name__ == "__main__":
     # pull_repos()
     # fixStars()
-    with open('data/go_Random400.json', 'r') as f:
-        data = json.load(f)
-        print(len(data['data']))
-
-        for i in range(0, len(data['data'])):
-            if not testGoReproducible([data['data'][i]['project'], data['data'][i]['url'], data['data'][i]['stars']], "go_Random400Results"):
-                continue
+    convert_csv()
+    # with open('data/go_Random400.json', 'r') as f:
+    #     data = json.load(f)
+    #     print(len(data['data']))
+    #
+    #     for i in range(0, len(data['data'])):
+    #         if not testGoReproducible([data['data'][i]['project'], data['data'][i]['url'], data['data'][i]['stars']], "go_Random400Results"):
+    #             continue
 
 
