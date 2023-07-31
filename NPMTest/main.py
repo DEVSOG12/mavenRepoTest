47f555fb57f963b38b66cc942bad790597b33bca
DEVSOG12 <oreofesolarin@gmail.com>
2023-07-31 10:49:07 -0400

    done fix

--- NPMTest/main.py
+++ NPMTest/main.py
@@ -9,7 +9,7 @@ possible_variations = ["environment", "build_path", "user_group", "fileordering"
                        "home", "kernel", "locales", "exec_path", "time", "timezone", "umask"]
 
 def run_reprotest(variation):
-    command = "sudo reprotest --variations=+{} ".format(variation) +
+    command = "sudo reprotest --variations=+{} ".format(variation) + \
               "'npm pack --pack-destination='./dest''" \
               "'dest/*.tgz'"
     print(command)
